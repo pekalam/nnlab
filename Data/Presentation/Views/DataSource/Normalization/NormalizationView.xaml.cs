998d058b3d65b3aa186ec687081f4bf41963160f
Marek Pękala <marekbf3@gmail.com>
2020-08-18 17:19:45 +0200

    added normalization view

---
+++ Data/Presentation/Views/DataSource/Normalization/NormalizationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Data.Presentation.Views.DataSource.Normalization
+{
+    /// <summary>
+    /// Interaction logic for NormalizationView
+    /// </summary>
+    public partial class NormalizationView : UserControl
+    {
+        public NormalizationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
